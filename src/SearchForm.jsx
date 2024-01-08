5d9f404056e6391e5993fa26875f8a93488c4935
Ayush Mishra <ayushmishra0092@gmail.com>
2024-01-08 13:36:21 +0530

    Final Commit

--- src/SearchForm.jsx
+++ src/SearchForm.jsx
@@ -12,7 +12,7 @@ const SearchForm = () => {
 
   return (
     <section>
-      <h1 className="title">Unsplash Images</h1>
+      <h1 className="title">Images</h1>
       <form className="search-form" onSubmit={handleSubmit}>
         <input
           type="text"
